9deeed3f065362c481100707bdda3f1364b95a16
0livia-L <55372967+0livia-L@users.noreply.github.com>
2019-10-06 20:24:27 +1100

    Cleaning up

--- svnjunfiubfvohrwolhneboihgrygrwrojgb.py
+++ svnjunfiubfvohrwolhneboihgrygrwrojgb.py
@@ -53,7 +53,7 @@ def function(numberOfRotations, speed, colourSensor):
     print("turns")
 
     #...................................................................................................
-    #...................................................................................................
+    prev_RLI = current_RLI
     #...................................................................................................
     
     while int(target_rotations) >= int(current_rotations):
@@ -62,10 +62,13 @@ def function(numberOfRotations, speed, colourSensor):
         
         if colourSensor == "RIGHT":
             current_RLI = colourRight.reflected_light_intensity
-            #print("Current COLOUR SENSOR RIGHT")
-
+            currentLeft_RLI = colourLeft.reflected_light_intensity            
+            
         if colourSensor == "LEFT":
             current_RLI = colourLeft.reflected_light_intensity
+            currentRight_RLI  = colourRight.reflected_light_intensity
+
+
         #______________________________________________________________________________
 
         error = target_RLI - current_RLI
@@ -74,17 +77,24 @@ def function(numberOfRotations, speed, colourSensor):
             print("Changed ERROR")
 
             
-        correction = error *1.01
+        correction = error *1
         print("Correction: {} Error: {}".format (correction,error))
         
         steering_drive.on(steering=-correction, speed=speed)
 
 
-    
+        if prev_RLI >= 120:
+            if currentLeft_RLI <= 30:
+                print("FOUND BLACK")
+                break
+            else:
+                continue
+        
         # Do this after we have moved.  If we haven't reached the target_rotations, it will repeat again.
         current_rotations = largeMotor_Left.position
-
+        prev_RLI = currentLeft_RLI
+        
     steering_drive.off()
     
 
-function(numberOfRotations = 2, speed = 15, colourSensor = "RIGHT" )
+function(numberOfRotations = 2, speed = 13, colourSensor = "RIGHT" )
