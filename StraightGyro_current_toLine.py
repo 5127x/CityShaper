8db0975cdcea0327842dddf569c3cc8bc948bb8c
simonjholmes <simon.holmes@adventone.com>
2019-10-28 11:02:27 +1100

    Added StraighGyro_current_toLine and StraightGyro_target_toLine

--- StraightGyro_current_toLine.py
+++ StraightGyro_current_toLine.py
@@ -13,13 +13,19 @@ tank_block = MoveTank(OUTPUT_B, OUTPUT_C)
 largeMotor_Left= LargeMotor(OUTPUT_B)
 largeMotor_Right= LargeMotor(OUTPUT_C)
 
-def StraightGyro_current(stop, speed, rotations):
-    print("In StraightGyro_current", file=stderr)
+colourLeft = ColorSensor(INPUT_3)
+colourRight = ColorSensor(INPUT_2)
+
+def StraightGyro_current_toLine(stop, speed, rotations, whiteOrBlack):
+
+    print("In StraightGyro_current_toLine", file=stderr)
+    
     current_degrees = largeMotor_Left.position 
     rotations = rotations * 360
     target_rotations= current_degrees + rotations
     target = gyro.angle
     current_gyro_reading = target
+
     # print("Current Gyro Reading: {}".format(current_gyro_reading))
     while float(current_degrees) < target_rotations:
         if stop(): 
@@ -40,8 +46,27 @@ def StraightGyro_current(stop, speed, rotations):
             break
         if stop():
             break
+
+    # Now find the line
+    
+    if not stop():
+        while True:
+            if stop(): 
+                break
+            currentRight_RLI = colourRight.reflected_light_intensity
+            currentLeft_RLI = colourLeft.reflected_light_intensity
+            steering_drive.on(steering = 0 , speed = speed / 2)
+
+            if whiteOrBlack == "WHITE":
+                if currentRight_RLI > 65 or currentLeft_RLI > 65:
+                    break
+
+            if whiteOrBlack == "BLACK":
+                if currentRight_RLI < 10 or currentLeft_RLI < 10:
+                    break
+
     tank_block.off()
-    print('Leaving StraightGyro_current', file=stderr)
+    print('Leaving StraightGyro_current_toLine', file=stderr)
 
 #stopProcessing=False
 #StraightGyro_current(lambda:stopProcessing, speed=30, rotations=3)
